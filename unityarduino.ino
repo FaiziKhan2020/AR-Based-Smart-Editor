daa8daf63e518055b1d9dd8067f0397053f2c6cd
FaiziKhan2020 <mfk.khan2020@gmail.com>
2016-12-26 15:33:04 +0500

    Add files via upload

---
+++ unityarduino.ino
@@ -0,0 +1,31 @@
+ int LED = 11;
+ String inString="";
+ int voltageLevel =0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED,OUTPUT);
+  while (!Serial) {
+ 
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available() > 0){
+   int inChar = Serial.read();
+    if (isDigit(inChar)) {
+      // convert the incoming byte to a char
+      // and add it to the string:
+      inString += (char)inChar;
+    }
+    // if you get a newline, print the string,
+    // then the string's value:
+    if (inChar == '\n') {
+      voltageLevel = inString.toInt();
+      inString = "";
+    }
+    }
+    else
+    analogWrite(LED,voltageLevel);
+}
